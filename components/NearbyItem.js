f88a6eaf749eefcd929ad2b7d1306817bcc9cf08
Hanif Ramadhan <hramadhaan20@gmail.com>
2023-01-15 08:46:17 +0700

    shared-elements

--- components/NearbyItem.js
+++ components/NearbyItem.js
@@ -1,17 +1,26 @@
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import React from 'react'
 import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { SharedElement } from 'react-navigation-shared-element'
 import { colors } from '../utils/Styles'
 
 function NearbyItem (props) {
   const { item, navigation } = props
   return (
-    <TouchableOpacity onPress={() => { navigation.navigate('DetailScreen') }} style={{ flexDirection: 'row', padding: 8, borderWidth: 1, borderColor: colors.greyLight, borderRadius: 8 }}>
-      <Image source={{ uri: item.images[0] }} style={{ width: 100, height: 100, borderRadius: 10 }} />
+    <TouchableOpacity onPress={() => { navigation.navigate('DetailScreen', { item }) }} style={{ flexDirection: 'row', padding: 8, borderWidth: 1, borderColor: colors.greyLight, borderRadius: 8 }}>
+      <SharedElement id={`${item.id}.img`}>
+        <Image source={{ uri: item.images[0] }} style={{ width: 100, height: 100, borderRadius: 10 }} />
+      </SharedElement>
       <View style={{ marginLeft: 10, flex: 1 }}>
-        <Text style={{ fontFamily: 'Sofia-Medium', color: colors.redDark, fontSize: 14 }}>{item.category}</Text>
-        <Text style={{ fontFamily: 'Sofia-Medium', color: colors.black, fontSize: 16, marginTop: 8 }}>{item.houseName}</Text>
-        <Text style={{ fontFamily: 'Sofia-Medium', color: colors.greyDark, fontSize: 14, marginTop: 6 }}>{item.address}</Text>
+        <SharedElement id={`${item.id}.category`}>
+          <Text style={{ fontFamily: 'Sofia-Medium', color: colors.redDark, fontSize: 14 }}>{item.category}</Text>
+        </SharedElement>
+        <SharedElement id={`${item.id}.name`}>
+          <Text style={{ fontFamily: 'Sofia-Medium', color: colors.black, fontSize: 16, marginTop: 8 }}>{item.houseName}</Text>
+        </SharedElement>
+        <SharedElement id={`${item.id}.address`}>
+          <Text style={{ fontFamily: 'Sofia-Medium', color: colors.greyDark, fontSize: 14, marginTop: 6 }}>{item.address}</Text>
+        </SharedElement>
         <View style={{ marginTop: 6, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
           <View style={{ flexDirection: 'row' }}>
             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
